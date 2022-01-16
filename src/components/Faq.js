239424a536dd03425e9715e7811a022ddbb3939d
Romulo Wylhames <blackwylhames1001@gmail.com>
2022-01-16 10:26:03 -0300

    finalizando a home

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col, Accordion, Card, Button } from "react-bootstrap";
 import IconText from "./IconText";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -9,96 +9,120 @@ import {
   faUserTie,
 } from "@fortawesome/free-solid-svg-icons";
 
-const Faq = () => (
-  <Container className="py-5">
-    <Row className="d-flex align-items-center justify-items-center">
-      <Col className="d-lg-none mb-5" xs={12}>
-        <Row>
-          <Col className='d-flex justify-content-center'>
-            <FontAwesomeIcon icon={faCreditCard} size="2x" />
+import "./Faq.scss";
+
+const Faq = () => {
+  const options = [
+    { icon: faCreditCard, text: "Cartão de crédito" },
+    { icon: faWallet, text: "Conta e abertura" },
+    { icon: faShieldAlt, text: "Token digital" },
+    { icon: faUserTie, text: "Produtos e serviços" },
+  ];
+
+  const [ index, setIndex ] = useState(0);
+
+  return (
+    <section className="faq text-light">
+      <Container className="py-5">
+        <Row className="text-center">
+          <h2 className="faq-title my-5">Dúvidas frequentes</h2>
+        </Row>
+        <Row className="d-flex align-items-center justify-items-center">
+          <Col className="d-lg-none mb-5" xs={12}>
+            <Row>
+              {options.map(({icon}, key) => (
+                <Col className="d-flex justify-content-center" key={key}>
+                  <FontAwesomeIcon icon={icon} size="2x" color={key === index ? '#FFF' : '#BBB'} onClick={() => setIndex(key)} />
+                </Col>
+              ))}
+            </Row>
+            <Row className="justify-content-center mt-5">
+              <p className="lead">{options[index].text}</p>
+            </Row>
           </Col>
-          <Col className='d-flex justify-content-center'>
-            <FontAwesomeIcon icon={faWallet} size="2x" />
+
+          <Col className="d-none d-lg-block">
+            {options.map(({ icon, text }, key) => (
+              <IconText
+              icon={icon}
+              size={3}
+              className="faq-clickble mb-3"
+              textClassName="lead"
+              color={key === index ? '#FFF' : '#BBB'}
+              onClick={() => setIndex(key)}
+              key={key}
+              >
+                {text}
+              </IconText>
+            ))}
           </Col>
-          <Col className='d-flex justify-content-center'>
-            <FontAwesomeIcon icon={faShieldAlt} size="2x" />
-          </Col>
-          <Col className='d-flex justify-content-center'>
-            <FontAwesomeIcon icon={faUserTie} size="2x" />
+              
+          <Col>
+            <Accordion defaultActiveKey="0" activeKey={`${index}`}>
+              <Accordion.Item eventKey="0" className="faq__card">
+                <Accordion.Header>Lorem ipsum dolor sit amet</Accordion.Header>
+                <Accordion.Body>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="1" className="faq__card">
+                <Accordion.Header>
+                  Nulla porttitor eros ac ex trístique posuere
+                </Accordion.Header>
+                <Accordion.Body>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="2" className="faq__card">
+                <Accordion.Header>
+                  Quisque fringilla tincidunt arcu
+                </Accordion.Header>
+                <Accordion.Body>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="3" className="faq__card">
+                <Accordion.Header>
+                  Nam elementum lobortis purus a maximus tortor
+                </Accordion.Header>
+                <Accordion.Body>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
           </Col>
         </Row>
-      </Col>
-
-      <Col className="d-none d-lg-block">
-        <IconText icon={faCreditCard} size={3} className="mb-3" textClassName="lead">
-          Cartão de crédito e débito
-        </IconText>
-        <IconText icon={faWallet} size={3} className="mb-3" textClassName="lead">
-          Conta e abertura
-        </IconText>
-        <IconText icon={faShieldAlt} size={3} className="mb-3" textClassName="lead">
-          Token digital
-        </IconText>
-        <IconText icon={faUserTie} size={3} textClassName="lead">
-          Produtos e serviços
-        </IconText>
-      </Col>
-
-      <Col>
-        <Accordion defaultActiveKey="0">
-          <Accordion.Item eventKey="0">
-            <Accordion.Header>Lorem ipsum dolor sit amet</Accordion.Header>
-            <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="1">
-            <Accordion.Header>Nulla porttitor eros ac ex trístique posuere</Accordion.Header>
-            <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="2">
-            <Accordion.Header>Quisque fringilla tincidunt arcu</Accordion.Header>
-            <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="3">
-            <Accordion.Header>Nam elementum lobortis purus a maximus tortor</Accordion.Header>
-            <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Accordion.Body>
-          </Accordion.Item>
-        </Accordion>
-        
-      </Col>
-    </Row>
-  </Container>
-);
+      </Container>
+    </section>
+  );
+};
 
 export default Faq;
